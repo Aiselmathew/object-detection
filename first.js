94848dc7c6d3e45219877cb2df7dd092b13dd7df
Aiselmathew <133201278+Aiselmathew@users.noreply.github.com>
2023-05-26 04:49:42 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,19 @@
+function changehtml() {
+    window.location="sec.html";
+}
+
+function htmlchange() {
+    window.location="third.html";
+}
+
+function html() {
+    window.location="four.html";
+}
+
+function change() {
+    window.location="five.html";
+}
+
+function clhmatnhge() {
+    window.location="six.html";
+}
